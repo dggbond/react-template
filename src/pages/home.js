86fd01da8549e211766aa2118cca008a1edf1daf
zhengmao <zhengmao0613@163.com>
2020-06-08 11:53:00 +0800

    Replace @emotion/core to emotion

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,14 @@
 import { Scrollbars } from "react-custom-scrollbars"
+import { css } from "emotion"
 
-const Home = () => <div>Home</div>
+const Home = () => {
+  return (
+    <Scrollbars style={{ height: 200, width: 100 }}>
+      <div className={ css`height: 500px; width: 100px;` }>
+        home
+      </div>
+    </Scrollbars>
+  )
+}
+
 export default Home
