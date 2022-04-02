31d21fb9fce88cc6bb9d8ccbbf996774daecd667
zhengmao <zhengmao0613@163.com>
2022-04-03 00:45:02 +0800

    chore: remove some packages no used and optimize routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react'
 import { renderRoutes } from 'react-router-config'
-import { useHistory } from 'react-router-dom'
-import { Button } from 'antd'
 
+import globalCtx from '~/context/global'
 import routes from '~/routes'
 
-import 'antd/dist/antd.css'
 import './App.less'
-import globalCtx from '~/context/global'
 
 const App: React.FC<{}> = () => {
-  const history = useHistory()
   const { user, setUser } = React.useContext(globalCtx)
 
   setTimeout(() => {
@@ -24,10 +20,6 @@ const App: React.FC<{}> = () => {
 
   return <>
     {renderRoutes(routes)}
-    <div>
-      <Button onClick={() => history.push('/pageA')}>to pageAAA</Button>
-      <Button onClick={() => history.push('/pageB')}>to pageB</Button>
-    </div>
   </>
 }
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react'
+import { useHistory } from 'react-router-dom'
+
 import globalCtx from '~/context/global'
 
 export default () => {
   const { theme, setTheme } = React.useContext(globalCtx)
+  const history = useHistory()
 
   return <div>
+    HOME
     Theme: {theme}
-    pageAFFF
     <br />
     <button onClick={() => setTheme('dark')}>set dark theme</button>
+    <button onClick={() => history.push('/foo')}>to page foo</button>
   </div>
 }
